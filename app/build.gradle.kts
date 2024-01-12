7ac0c937934a814cb8c3a4d7998b7d0663205b4a
Joshua Kuestersteffen <jkuester@kuester7.com>
2024-01-12 08:44:18 -0600

    chore: prepare for next release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.jkuester.unlauncher"
         minSdk = 21
         targetSdk = 34
-        versionName = "2.1.0"
-        versionCode = 20
+        versionName = "2.2.0-beta.1"
+        versionCode = 21
         vectorDrawables { useSupportLibrary = true }
 //        signingConfigs {
 //            if (project.extra.has("RELEASE_STORE_FILE")) {
